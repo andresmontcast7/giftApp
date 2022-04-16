2f490c25a405cfb521bd7c8e3262217c301a7122
Andres Monterroso <andresmontcast@icloud.com>
2022-04-16 11:47:08 -0600

    add docs

---
+++ docs/precache-manifest.2efb6b5c1461bfa07c67f5e22e4189b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368f34ffc4df471d3c9a6c29bbce4b11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f44cd405087661ce3e",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f44cd405087661ce3e",
+    "url": "/static/js/main.df24bd01.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
